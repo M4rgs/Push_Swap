c8b752d923cba6e256832f8f0ae7985392bdc0ec
Taha Mounir <tamounir@e1r9p9.1337.ma>
2025-02-16 06:11:59 +0100

    pshshw

--- instruc_a.c
+++ instruc_a.c
@@ -6,7 +6,7 @@
 /*   By: tamounir <tamounir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/16 04:46:47 by tamounir          #+#    #+#             */
-/*   Updated: 2025/02/16 05:35:28 by tamounir         ###   ########.fr       */
+/*   Updated: 2025/02/16 06:11:55 by tamounir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,4 +21,23 @@ void	sa(t_argus *argus)
 	argus->stack_a[0] = argus->stack_a[1];
 	argus->stack_a[1] = tmp;
 	ft_putstr_fd("sa\n", 1);
+}
+
+void	rra(t_argus *argus)
+{
+	int	i;
+	int	l;
+
+	if (argus->a_len > 1)
+	{
+		i = argus->a_len;
+		l = argus->stack_a[argus->a_len - 1];
+		while (i > 0)
+		{
+			argus->stack_a[i] = argus->stack_a[i - 1];
+			i--;
+		}
+		argus->stack_a[0] = l;
+		ft_putstr_fd("rra\n", 1);
+	}
 }
